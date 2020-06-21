f58540a9a2984a2eef70d7d02fefa82d18249fec
Anupam <onlyprogramming000@gmail.com>
2020-06-22 02:24:15 +0530

    Added title and body property

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note!",
-  handler: function () {
-    console.log(chalk.blue("Adding a new note!!"));
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title:" + argv.title);
+    console.log("Body:" + argv.body);
   },
 });
 
@@ -41,5 +54,5 @@ yargs.command({
   },
 });
 
-// Commands: Add, Remove, Read, Listen
+//yargs.parse();
 console.log(yargs.argv);
