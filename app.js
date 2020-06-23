54536f4726cdb7646f73acc6b140e2672943caaa
Anupam <onlyprogramming000@gmail.com>
2020-06-23 15:42:14 +0530

    Added remove notes function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -22,8 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title:" + argv.title);
-    console.log("Body:" + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
@@ -31,8 +30,15 @@ yargs.command({
 yargs.command({
   command: "remove",
   describe: "Remove a note!",
-  handler: function () {
-    console.log(chalk.red("Removing a note!!"));
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
@@ -54,5 +60,5 @@ yargs.command({
   },
 });
 
-//yargs.parse();
-console.log(yargs.argv);
+yargs.parse();
+// console.log(yargs.argv);
